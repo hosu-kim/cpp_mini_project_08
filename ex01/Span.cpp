cacfe5cbb0772134d1914d8007b4acf22cfc1614
hosu-kim <hosu@outlook.cz>
2026-03-11 21:47:15 +0100

    commit

--- ex01/Span.cpp
+++ ex01/Span.cpp
@@ -1,5 +1,6 @@
 #include "Span.hpp"
 
+// orthodox canonical form
 Span::Span() : _maxSize(0) {}
 
 Span::Span(unsigned int n) : _maxSize(n) {
@@ -19,9 +20,12 @@ Span& Span::operator=(const Span& src) {
 }
 
 Span::~Span() {}
+// ==================================================================
 
 void Span::addNumber(int n) {
 	if (_numbers.size() >= _maxSize) {
+		// std::out_of_range: deault exception class;
+		// used when the program attemps to access outside the specified range.
 		throw std::out_of_range("Error: Span is already full!");
 	}
 	_numbers.push_back(n);
@@ -29,13 +33,19 @@ void Span::addNumber(int n) {
 
 
 int Span::shortestSpan() {
+	// only one number exists
 	if (_numbers.size() < 2)
 		throw std::logic_error("Not enough numbers to find a span");
 	
 	std::vector<int> sorted = _numbers;
+	// std::sort: sorts data in ascending order
+	// begin(): gets the first element <=> end()
 	std::sort(sorted.begin(), sorted.end());
 
+	// sets initial value.
 	int minDiff = sorted[1] - sorted[0];
+	// why sortd.size() '- 1'? at the end, these is only one number left,
+    //                         so there is nothing to compare it to.
 	for (size_t i = 1; i < sorted.size() - 1; ++i) {
 		int diff = sorted[i + 1] - sorted[i];
 		if (diff < minDiff)
@@ -45,9 +55,12 @@ int Span::shortestSpan() {
 }
 
 int Span::longestSpan() {
+	// only one number exists
 	if (_numbers.size() < 2)
 		throw std::logic_error("Not enough numbers to find a span");
 
+	// why '*'std::min_element? the function return an iterator.
+	//                          you need to take int value to compare with max.
 	int minVal = *std::min_element(_numbers.begin(), _numbers.end());
 	int maxVal = *std::max_element(_numbers.begin(), _numbers.end());
 
--- ex01/Span.hpp
+++ ex01/Span.hpp
@@ -6,6 +6,9 @@
 #include <algorithm>
 #include <exception>
 
+// DOC: Develop a Span class that can store a maximum of N integers.
+//      N is an unsigned int variable and will be the only parameter passed
+//      to the constructor.
 class Span {
 	private:
 		unsigned int _maxSize;
@@ -18,15 +21,25 @@ class Span {
 		Span& operator=(const Span& other);
 		~Span();
 
+		// DOC: addNumber() to add a single number to the Span.
+		// It will be used in order to fill it.
 		void addNumber(int n);
 
+// DOC: Implement a member function to add multiple numbers
+// to your Span in a single call.
+
+// Template functions must be implemented immediately upon declaration.
 		template <typename T>
+		// T begin, T end: a range of numbers from a vector/list
 		void addNumbers(T begin, T end) {
+			// std::distance: calculates how many numbers there are 
+			//                in the range of data from a vector/list.
 			if (_numbers.size() + std::distance(begin, end) > _maxSize)
 				throw std::out_of_range("Span is full");
 			_numbers.insert(_numbers.end(), begin, end);
 		}
 
+		// DOC: They will respectively find out the shortest span or the longest span
 		int shortestSpan();
 		int longestSpan();
 };
